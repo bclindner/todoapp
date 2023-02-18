b8dc5ec05157cf37092f9526f1ca5f8dfbb52546
Brian Lindner <brian@bclindner.com>
2023-02-17 22:15:11 -0500

    general simplification
    the defaults for exceptions are actually fine - it was hard to make it
    out due to spring security masking most of these errors with 401s

--- src/main/java/com/bclindner/todo/exception/ResourceExistsException.java
+++ src/main/java/com/bclindner/todo/exception/ResourceExistsException.java
@@ -1,8 +1,12 @@
 package com.bclindner.todo.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * General exception to throw if resources already exist (when the request
  * requires that they do not).
  */
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class ResourceExistsException extends RuntimeException {
 }
--- src/main/java/com/bclindner/todo/exception/ResourceNotFoundException.java
+++ src/main/java/com/bclindner/todo/exception/ResourceNotFoundException.java
@@ -1,8 +1,12 @@
 package com.bclindner.todo.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * General exception to throw if a resource does not exist when the user is
  * trying to retrieve it.
  */
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 }
