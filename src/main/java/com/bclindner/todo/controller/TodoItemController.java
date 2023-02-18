ff5f1d78e5a46210b6d98b0a1a5b2d4ff47d70e8
Brian Lindner <brian@bclindner.com>
2023-02-18 15:21:08 -0500

    full test suite completed (for now)

--- src/main/java/com/bclindner/todo/controller/TodoItemController.java
+++ src/main/java/com/bclindner/todo/controller/TodoItemController.java
@@ -6,6 +6,7 @@ import java.util.Collection;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -186,11 +188,12 @@ public class TodoItemController {
         description = "Delete a TodoItem by its ID.",
         responses = {
             @ApiResponse(
-                responseCode = "200",
+                responseCode = "204",
                 description = "Deletion successful"
             )
         }
     )
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteTodo(@PathVariable Long id) {
         service.deleteById(id);
     }
--- src/test/java/com/bclindner/todo/TodoItemControllerTests.java
+++ src/test/java/com/bclindner/todo/TodoItemControllerTests.java
@@ -1,14 +1,22 @@
 package com.bclindner.todo;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.util.Optional;
+
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.bclindner.todo.model.TodoItem;
+import com.bclindner.todo.service.TodoItemService;
 
 /**
  * General MockMvc integration tests for the API.
@@ -17,24 +25,145 @@ import org.springframework.test.web.servlet.MockMvc;
  * are up to my personal spec.
  */
 @SpringBootTest
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 public class TodoItemControllerTests {
+
+    // Request tester
     @Autowired
     private MockMvc mvc;
     
+    // Service (for adding/testing data)
+    @Autowired
+    private TodoItemService todoItemService;
+    
+    /**
+     * Test the empty GET request.
+     * @throws Exception
+     */
     @Test
-    public void testGet() throws Exception {
+    public void testGetEmpty() throws Exception {
         mvc.perform(
             get("/todo-item/")
             .contentType("application/json"))
-            .andExpect(status().isOk());
-        // apparently, for security reasons, Spring 6 deprecated the trailing
-        // slash ignore as it caused issues w/ Spring Security edgecases that
-        // led to vulns, so that part is tested here
+            .andExpect(status().isNoContent());
+    }
+    
+    /**
+     * Test that the /todo-item/ endpoint returns 404 properly if no trailing
+     * slash is specified.
+     *
+     * I don't like this behavior personally - the trailing slash should be
+     * neither here nor there when requesting a REST collection - but
+     * apparently, for security reasons, Spring 6 deprecated the trailing slash
+     * ignore as a default behavior as it caused issues w/ Spring Security
+     * edgecases that led to vulns:
+     *
+     * https://github.com/spring-projects/spring-framework/issues/28552
+     * 
+     * In order to solve this problem, a custom PathPatternParser configuration
+     * would need to be set up, but given the security reasons for this change,
+     * I'm not sure that's a good idea.
+     *
+     * As such, we will abide by this recommendation (begrudgingly) and test it
+     * here.
+     * @throws Exception
+     */
+    @Test
+    public void testTrailingSlashRequired() throws Exception {
+        mvc.perform(
+            get("/todo-item")
+            .contentType("application/json"))
+            .andExpect(status().is(404));
+    }
+    
+    /**
+     * Test creation of a TodoItem resource.
+     * @throws Exception
+     */
+    @Test
+    @DirtiesContext
+    public void testCreate() throws Exception {
         mvc.perform(
             post("/todo-item/")
-            .contentType("application/json"))
-            .andExpect(status().is(400));
-
+            .contentType("application/json")
+            .accept("application/json")
+            .content("{\"text\": \"To-do item\"}")
+        )
+        .andExpect(status().isCreated())
+        .andExpect(content().contentType("application/json"))
+        .andExpect(jsonPath("$.id").value(1))
+        .andExpect(jsonPath("$.text").value("To-do item"))
+        .andExpect(jsonPath("$.completed").value(false))
+        .andExpect(header().string("Location", Matchers.containsString("/1"))); // may want to match this better later
+        // check service
+        assertNotNull(todoItemService.getById(1L).get(), "TodoItem doesn't exist in service");
     }
+
+    /**
+     * Test access to a TodoItem resource.
+     * @throws Exception
+     */
+    @Test
+    @DirtiesContext
+    public void testGet() throws Exception {
+        // add to service
+        TodoItem todoItem = todoItemService.save(new TodoItem("testGet"));
+        mvc.perform(
+            get("/todo-item/")
+            .accept("application/json")
+        )
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.[0].id").value(todoItem.getId()));
+    }
+    
+    
+    
+    /**
+     * Test update of a TodoItem resource.
+     * @throws Exception
+     */
+    @Test
+    @DirtiesContext
+    public void testUpdate() throws Exception {
+        // add to service
+        TodoItem todoItem = todoItemService.save(new TodoItem("testUpdate"));
+        String todoIdString = todoItem.getId().toString();
+        mvc.perform(
+            put("/todo-item/" + todoIdString)
+            .contentType("application/json")
+            .accept("application/json")
+            .content("{\"id\": " + todoIdString + ", \"text\": \"To-do item!\", \"completed\": true, \"createdDate\": null, \"lastModifiedDate\": null}")
+        )
+        .andExpect(status().isOk())
+        .andExpect(content().contentType("application/json"))
+        .andExpect(jsonPath("$.id").value(todoItem.getId()))
+        .andExpect(jsonPath("$.text").value("To-do item!"))
+        .andExpect(jsonPath("$.completed").value(true));
+        // check the service
+        Optional<TodoItem> retrievedTodoItem = todoItemService.getById(todoItem.getId());
+        assertTrue(retrievedTodoItem.isPresent(), "TodoItem doesn't exist in service");
+        TodoItem retrievedTodoItemUnwrapped = retrievedTodoItem.get();
+        assertEquals("To-do item!", retrievedTodoItemUnwrapped.getText(), "TodoItem was not properly updated");
+    }
+    
+    /**
+     * Test deletion of a TodoItem resource.
+     * @throws Exception
+     */
+    @Test
+    @DirtiesContext
+    public void testDelete() throws Exception {
+        // add to service
+        TodoItem todoItem = todoItemService.save(new TodoItem("testDelete"));
+        // run DELETE
+        mvc.perform(
+            delete("/todo-item/1")
+        )
+        .andExpect(status().isNoContent());
+        // check the service
+        Optional<TodoItem> retrievedTodoItem = todoItemService.getById(todoItem.getId());
+        assertFalse(retrievedTodoItem.isPresent(), "TodoItem still exists in service after deletion");
+    }
+    
 }
