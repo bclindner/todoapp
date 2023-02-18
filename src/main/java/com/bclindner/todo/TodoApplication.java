4519f581fce3a14a719db691084758fafb95244b
Brian Lindner <brian@bclindner.com>
2023-02-18 13:55:37 -0500

    configured OpenAPI bean; we now have valid OpenAPI spec

--- src/main/java/com/bclindner/todo/TodoApplication.java
+++ src/main/java/com/bclindner/todo/TodoApplication.java
@@ -1,9 +1,14 @@
 package com.bclindner.todo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @SpringBootApplication
 @EnableJpaRepositories // needed for @Entity
@@ -12,6 +17,22 @@ public class TodoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
+	}
+	
+	/**
+	 * Spec for the OpenAPI.
+	 * @param appVersion
+	 * @return
+	 */
+	@Bean
+	public OpenAPI openAPISpec(@Value("${springdoc.version}") String appVersion) {
+		return new OpenAPI()
+			.info(
+				new Info()
+					.title("To-Do App")	
+					.description("Primitive to-do list API.")
+					.version(appVersion)
+			);
 	}
 
 }
